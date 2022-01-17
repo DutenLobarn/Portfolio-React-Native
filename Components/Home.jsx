520a121c6ba4e09aa310e2ec4397a656f22d3afa
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2022-01-17 17:04:02 +0100

    update homes css

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -6,97 +6,115 @@ import { Text, View, ImageBackground, StyleSheet } from "react-native";
 export default function Home() {
   return (
     <View style={styles.container}>
+
+
       <ImageBackground
         style={styles.image}
         source={require("../assets/src/hero.png")}
         resizeMode="cover"
       >
-        <Entypo
-          style={{
-            display: "flex",
-            width: "33%",
-            height: "10%",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          name="sweden"
-          size={55}
-          color="blue"
-        />
-        <View
-          style={{
-            alignItems: "center",
-            height: "25%",
-            justifyContent: "center",
-            marginTop: 45,
-            marginRight: 40,
-            marginLeft: 40,
-            marginBottom: 0,
-          }}
-        >
-          <Text
+        <View style={{
+          width: "100%",
+          height: "100%",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          paddingTop: 30,
+        }}>
+          <Entypo
             style={{
-              fontSize: 30,
-              color: "#7C2929",
-              fontFamily: "fantasy",
-              marginBottom: 15,
+              display: "flex",
+              /*     width: "33%", */
+              /*     height: "10%", */
+              /*  justifyContent: "center",
+               alignItems: "center", */
+            }}
+            name="sweden"
+            size={55}
+            color="blue"
+          />
+          <View
+            style={{
+              paddingTop: 50,
+              alignItems: "center",
+
+              /*  justifyContent: "center", */
+              /* marginTop: 45,
+              marginRight: 40,
+              marginLeft: 40,
+              marginBottom: 0, */
             }}
           >
-            Mandus
-          </Text>
-          <Text
+            <Text
+              style={{
+                fontSize: 30,
+                color: "#7C2929",
+                fontFamily: "fantasy",
+                paddingBottom: 20,
+                /*   marginBottom: 15, */
+              }}
+            >
+              Mandus
+            </Text>
+            <Text
+              style={{
+                fontSize: 25,
+                color: "#7C2929",
+                fontFamily: "fantasy",
+
+              }}
+            >
+              FrontEnd
+            </Text>
+            <Text
+              style={{
+                fontSize: 25,
+                color: "#7C2929",
+                fontFamily: "fantasy",
+                lineHeight: 38,
+              }}
+            >
+              Web och App
+            </Text>
+            <Text
+              style={{
+                fontSize: 25,
+                color: "#7C2929",
+                fontFamily: "fantasy",
+              }}
+            >
+              Utveckling
+            </Text>
+          </View>
+          <Ionicons
             style={{
-              fontSize: 25,
-              color: "#7C2929",
-              fontFamily: "fantasy",
+              /* display: "flex",
+              width: "33%",
+              height: "10%", */
+              /*  justifyContent: "center", */
+              /*     fontSize: "xxx-large", */
+              /*  alignItems: "center", */
             }}
-          >
-            FrontEnd
-          </Text>
-          <Text
-            style={{
-              fontSize: 25,
-              color: "#7C2929",
-              fontFamily: "fantasy",
-              lineHeight: 38,
-            }}
-          >
-            Web och App
-          </Text>
-          <Text
-            style={{
-              fontSize: 25,
-              color: "#7C2929",
-              fontFamily: "fantasy",
-            }}
-          >
-            Utveckling
-          </Text>
+            name="sunny"
+            size={55}
+            color="#7C2929"
+          />
+
         </View>
-        <Ionicons
-          style={{
-            display: "flex",
-            width: "33%",
-            height: "10%",
-            justifyContent: "center",
-            fontSize: "xxx-large",
-            alignItems: "center",
-          }}
-          name="sunny"
-          size={30}
-          color="#7C2929"
-        />
       </ImageBackground>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    height: "100%",
+    flex: 1,
+    /* height: "100%", */
   },
   image: {
-    display: "flex",
-    height: "100%",
-    flexDirection: "row",
+    flex: 1,
+    /*  display: "flex", */
+
+    /*  height: "100%", */
+    /*  flexDirection: "row", */
+    /*  justifyContent: "center" */
   },
 });

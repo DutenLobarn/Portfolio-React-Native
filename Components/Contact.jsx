0d92f5da7cc6c1c131e4e9b957d5d8b6c92c0608
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2022-01-21 12:34:45 +0100

    Update Contact.jsx

--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -1,11 +1,46 @@
-import React from "react";
-import { Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Image, Button, TextInput } from "react-native";
 import AppLoading from "expo-app-loading";
 import { LinearGradient } from "expo-linear-gradient";
 import { useFonts, Orbitron_500Medium } from "@expo-google-fonts/orbitron";
-import { Octicons } from "@expo/vector-icons";
 
 export default function Contact({ colorTheme, title }) {
+  const [firstName, setFirstName] = useState("");
+  const [Lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  console.log(firstName);
+  console.log(Lastname);
+  console.log(email);
+  console.log(message);
+
+  const textInput = (text, setText) => {
+    return (
+      <TextInput
+        placeholder={text}
+        placeholderTextColor={
+          colorTheme === "#000" ? "#rgba(0, 0 ,0, 0.5)" : "#8F2F2F"
+        }
+        onChangeText={(text) => {
+          {
+            setText;
+          }
+          text;
+        }}
+        style={{
+          backgroundColor: "grey",
+          padding: 10,
+          width: "80%",
+          alignSelf: "center",
+          textAlign: "center",
+          borderRadius: 10,
+          marginTop: 10,
+        }}
+      />
+    );
+  };
+
   let mainLinearGradientColor = "#000";
   let secondaryLinearGradientColor = "grey";
   if (colorTheme === "#000") {
@@ -56,6 +91,45 @@ export default function Contact({ colorTheme, title }) {
               height: 100,
             }}
             source={require("../assets/src/mail.png")}
+          />
+        </View>
+
+        <View>
+          {textInput("Firstname", setFirstName)}
+          {textInput("Lastname", setLastName)}
+
+          <TextInput
+            placeholder="Email"
+            onChangeText={(text) => {
+              setEmail(text);
+            }}
+            style={{
+              backgroundColor: "grey",
+              padding: 10,
+              width: "80%",
+              alignSelf: "center",
+              textAlign: "center",
+              borderRadius: 10,
+              marginTop: 10,
+            }}
+          />
+
+          <TextInput
+            placeholder="Message"
+            onChangeText={(text) => {
+              setMessage(text);
+            }}
+            style={{
+              backgroundColor: "grey",
+              paddingTop: 10,
+              paddingBottom: 50,
+              paddingLeft: 10,
+              paddingRight: 10,
+              width: "80%",
+              alignSelf: "center",
+              borderRadius: 10,
+              marginTop: 10,
+            }}
           />
         </View>
       </LinearGradient>

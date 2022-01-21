eda17dca172d1d4a94c4122cb583b10bd5a9ec47
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2022-01-21 10:50:36 +0100

    import image

--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -1,21 +1,64 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+import AppLoading from "expo-app-loading";
+import { LinearGradient } from "expo-linear-gradient";
+import { useFonts, Orbitron_500Medium } from "@expo-google-fonts/orbitron";
+import { Octicons } from "@expo/vector-icons";
 
 export default function Contact({ colorTheme, title }) {
-  return (
-    <View>
-      <Text
-        style={{
-          position: "absolute",
-          top: 49,
-          alignSelf:"center",
-          fontSize: 30,
-          color: colorTheme,
-          fontWeight: "bold",
-        }}
+  let mainLinearGradientColor = "#000";
+  let secondaryLinearGradientColor = "grey";
+  if (colorTheme === "#000") {
+    mainLinearGradientColor = "#fff";
+    secondaryLinearGradientColor = "grey";
+  }
+
+  let [fontsLoaded] = useFonts({
+    Orbitron_500Medium,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <LinearGradient
+        style={{ flex: 1 }}
+        colors={[mainLinearGradientColor, secondaryLinearGradientColor]}
       >
-        {title}
-      </Text>
-    </View>
-  );
+        <Text
+          style={{
+            paddingTop: 50,
+            fontSize: 30,
+            color: colorTheme,
+            fontWeight: "bold",
+            textAlign: "center",
+            fontFamily: "Orbitron_500Medium",
+          }}
+        >
+          {title}
+        </Text>
+
+        <View
+          style={{
+            backgroundColor: colorTheme === "#000" ? "grey" : "#8F2F2F",
+            width: 180,
+            height: 180,
+            alignSelf: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 20,
+            borderRadius: "100%",
+          }}
+        >
+          <Image
+            style={{
+              width: 100,
+              height: 100,
+            }}
+            source={require("../assets/src/mail.png")}
+          />
+        </View>
+      </LinearGradient>
+    );
+  }
 }
